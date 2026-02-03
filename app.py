82fff26fa8dd6adcdefcba35365df5e04bff3222
hamed <hamedeg125@gmail.com>
2026-02-03 21:29:53 +0330

    first draft

---
+++ app.py
@@ -0,0 +1,44 @@
+import random
+
+WORDS = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+
+print('Welcome to Word Gussing Game!')
+
+name = input('What\'s your name? ')
+print(f'Good luck! {name}')
+
+word = random.choice(WORDS)
+
+print('Guess the characters.')
+
+guesses = ''
+turn = 12
+
+
+while turn > 0:
+    failed = 0
+
+    for i in word:
+        if i in guesses:
+            print(i,end=" ")
+        else:
+            print('_')
+            failed +=1
+
+    if failed == 0 :
+        print('You won!')
+        print(f'The word is : {word}')
+        break
+
+    guess =input('guess a character: ')
+    guesses += guess
+
+    if guess not in word:
+        turn -= 1
+        print('Wrong!')
+        print(f'You have {turn} more guesses.')
+
+    if turn == 0:
+        print('You loose')
